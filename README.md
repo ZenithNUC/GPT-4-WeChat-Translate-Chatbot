167c0e7c06d661e02527f8ca599935b4cd1c3064
ZenithNUC <lizengzhi1998@foxmail.com>
2023-05-04 15:25:19 +0800

    version:1.0

---
+++ README.md
@@ -0,0 +1,80 @@
+# GPT-4 WeChat Translate Chatbot
+
+一个基于OpenAI GPT-4的微信翻译机器人，为用户提供文本翻译功能。
+
+## 功能
+
+私聊中自动翻译用户内容至目标语言
+群聊中通过设定唤醒关键词激活机器人回复
+支持重置聊天上下文
+支持通过配置文件自定义参数
+
+## 环境要求
+
+Node.js >= 14
+
+TypeScript
+
+## 安装与运行
+克隆本项目至本地：
+
+下载代码
+```bash
+git clone https://github.com/yourusername/your-repo.git
+```
+进入项目目录：
+
+```bash
+Copy code
+cd your-repo
+```
+安装依赖：
+
+```bash
+npm install
+```
+
+在src/config.json中修改其中的配置参数，例如API密钥和目标语言等。
+
+编译TypeScript代码：
+
+```bash
+npm run build
+```
+
+运行项目：
+
+```bash
+npm start
+```
+
+这将启动微信机器人，扫描二维码登录后即可开始使用。
+
+## 配置文件说明
+
+在config.json文件中，您可以设置以下参数：
+
+OPENAI_API_KEY: 您的OpenAI API密钥
+
+reverseProxyUrl: 反向代理地址，如果需要使用，请填写您的代理服务器地址
+
+groupKey: 群聊中唤醒机器人的关键词
+
+privateKey: 私聊中唤醒机器人的关键词（可留空）
+
+resetKey: 重置聊天上下文的关键词
+
+groupReplyMode: 是否在群聊中带上提问的问题（布尔值，true或false）
+
+privateReplyMode: 是否在私聊中带上提问的问题（布尔值，true或false）
+
+transLanguage: 要翻译的目标语言（如法语）
+
+## 注意事项
+
+使用时请遵守相关法律法规，尊重用户隐私，合理使用API。
+本项目仅供学习和研究使用，不得用于任何商业用途。
+
+## 开源许可
+
+本项目采用MIT许可协议，项目基于[ChatGPT-wechat-bot](!https://github.com/AutumnWhj/ChatGPT-wechat-bot)进行修改
---
+++ src/index.ts
@@ -0,0 +1,108 @@
+import { WechatyBuilder } from "wechaty";
+import qrcodeTerminal from "qrcode-terminal";
+import config from "./config.json";
+import ChatGPT from "./chatgpt.js";
+
+const Prompt = "你现在是一个翻译，不论发给你什么内容，只需要将输入内容原意翻译成指定的语言，不需要任何建议性的回答，现在请翻译成";
+const Language = config.transLanguage;
+const globalPrompt = "以下是输入内容：";
+
+let bot: any = {};
+const startTime = new Date();
+let chatGPTClient: any = null;
+initProject();
+async function onMessage(msg) {
+  // 避免重复发送
+  if (msg.date() < startTime) {
+    return;
+  }
+  const contact = msg.talker();
+  const receiver = msg.to();
+  const content = msg.text().trim();
+  const room = msg.room();
+  const alias = (await contact.alias()) || (await contact.name());
+  const isText = msg.type() === bot.Message.Type.Text;
+  if (msg.self()) {
+    return;
+  }
+
+  if (room && isText) {
+    const topic = await room.topic();
+    console.log(
+      `Group name: ${topic} talker: ${await contact.name()} content: ${content}`
+    );
+
+    const pattern = RegExp(`^@${receiver.name()}\\s+${config.groupKey}[\\s]*`);
+    if (await msg.mentionSelf()) {
+      if (pattern.test(content)) {
+        const groupContent = content.replace(pattern, "");
+        chatGPTClient.replyMessage(room, groupContent);
+        return;
+      } else {
+        console.log(
+          "Content is not within the scope of the customizition format"
+        );
+      }
+    }
+  } else if (isText) {
+    console.log(`talker: ${alias} content: ${content}`);
+    if (content.startsWith(config.privateKey) || config.privateKey === "") {
+      let privateContent = content;
+      if (config.privateKey === "") {
+        privateContent = content.substring(config.privateKey.length).trim();
+      }
+      const contentWithPrompt = Prompt + Language + globalPrompt + privateContent;
+      chatGPTClient.replyMessage(contact, contentWithPrompt);
+    } else {
+      console.log(
+        "Content is not within the scope of the customizition format"
+      );
+    }
+  }
+}
+
+function onScan(qrcode) {
+  qrcodeTerminal.generate(qrcode, { small: true }); // 在console端显示二维码
+  const qrcodeImageUrl = [
+    "https://api.qrserver.com/v1/create-qr-code/?data=",
+    encodeURIComponent(qrcode),
+  ].join("");
+
+  console.log(qrcodeImageUrl);
+}
+
+async function onLogin(user) {
+  console.log(`${user} has logged in`);
+  const date = new Date();
+  console.log(`Current time:${date}`);
+}
+
+function onLogout(user) {
+  console.log(`${user} has logged out`);
+}
+
+async function initProject() {
+  try {
+    chatGPTClient = new ChatGPT();
+    bot = WechatyBuilder.build({
+      name: "WechatEveryDay",
+      puppet: "wechaty-puppet-wechat", // 如果有token，记得更换对应的puppet
+      puppetOptions: {
+        uos: true,
+      },
+    });
+
+    bot
+      .on("scan", onScan)
+      .on("login", onLogin)
+      .on("logout", onLogout)
+      .on("message", onMessage);
+
+    bot
+      .start()
+      .then(() => console.log("Start to log in wechat..."))
+      .catch((e) => console.error(e));
+  } catch (error) {
+    console.log("init error: ", error);
+  }
+}
